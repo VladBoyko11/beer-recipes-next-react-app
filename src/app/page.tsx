de03574fdc084f13a5b4db24cf7aab797c00e0ba
Vladislav Boyko <vlboyko11@gmail.com>
2023-07-30 12:10:51 +0300

    h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="font-medium">Hello world!</div>
+      <h1>Hello world!</h1>
       <Link href='/beer-recipes'>Beer Recipes</Link>
     </main>
   )
