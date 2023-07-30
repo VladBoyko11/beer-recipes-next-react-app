6298665e31533f6d2aecc6c9e97858742b035fa0
Vladislav Boyko <vlboyko11@gmail.com>
2023-07-30 12:25:45 +0300

    work correctly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="font-medium">Hello world!</div>
-      <h1>Hello worlddawdw!</h1>
+      <h1>Hello world!</h1>
       <Link href='/beer-recipes'>Beer Recipes</Link>
     </main>
   )
