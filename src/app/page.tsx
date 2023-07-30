0b6b64bc080f44e97528c38c33e36c2cadae50a5
Vladislav Boyko <vlboyko11@gmail.com>
2023-07-30 12:30:40 +0300

    hot-fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="font-medium">Hello world!</div>
       <h1>Hello world!</h1>
       <Link href='/beer-recipes'>Beer Recipes</Link>
     </main>
