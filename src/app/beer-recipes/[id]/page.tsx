be8800ba10b5b1c04c1cbb60dd0b0e7cdc61cd0c
VladBoyko11 <vlboyko11@gmail.com>
2023-07-31 22:20:56 +0300

    change style on beer page

--- src/app/beer-recipes/[id]/page.tsx
+++ src/app/beer-recipes/[id]/page.tsx
@@ -28,16 +28,16 @@ export default async function BeerRecipePage ({params: {id}}: Props) {
     const beerRecipe: Array<beerRecipe> = await getBeerRecipes(id)
 
     return <div className='container d-flex justify-content-between' >
-        <div>Beer Page {beerRecipe[0].name}</div>
+        <div className=" text-lg text-fuchsia-400">Beer name: {beerRecipe[0].name}</div>
         <div className='w-25'>
             <img className='m-3' src={beerRecipe[0].image_url} width="100px" alt="beer recipe" />
         </div>
         <div className='w-75'>
             <div className=''><strong>Recipe</strong>: {beerRecipe[0].name}</div>
-            <div className='font-italic'>{beerRecipe[0].tagline}</div>
-            <div><strong>First brewed</strong>: {beerRecipe[0].first_brewed}</div>
-            <div><strong>Alcohol by Volume</strong>: {beerRecipe[0].abv}</div>
-            <div><strong>Beer color intensity</strong>: {beerRecipe[0].srm}</div>
+            <div className=' italic text-fuchsia-200'>{beerRecipe[0].tagline}</div>
+            <div><strong>First brewed</strong>: <span className=" text-fuchsia-200">{beerRecipe[0].first_brewed}</span></div>
+            <div><strong>Alcohol by Volume</strong>: <span className=" text-fuchsia-200">{beerRecipe[0].abv}</span></div>
+            <div><strong>Beer color intensity</strong>: <span className=" text-fuchsia-200">{beerRecipe[0].srm}</span></div>
             {/* <div><strong>Fermentation at</strong>: {beerRecipe.method.fermentation.temp.value} degrees {beerRecipe.method.fermentation.temp.unit}</div>
             <hr />
             <div><strong>Food pairing</strong>:
