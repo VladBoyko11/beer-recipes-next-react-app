7c02236c1029daf05f2cd9d5e3bcaaa807300c45
VladBoyko11 <vlboyko11@gmail.com>
2023-07-31 22:09:35 +0300

    styles for beer recipes page

--- src/app/beer-recipes/page.tsx
+++ src/app/beer-recipes/page.tsx
@@ -89,15 +89,15 @@ export default async function BeerRecipes({params}: {params: any}) {
 
     const beerRecipes: Array<beerRecipe> = await getBeerRecipes()
     return <div>
-        <h1>BeerRecipes:</h1>
-        <ul>
+        <h1 className=" text-lg text-fuchsia-400">BeerRecipes:</h1>
+        <ol className=" ml-2 ">
             {beerRecipes.map((item: beerRecipe, index: number) => 
-            <li key={index}>
+            <li key={index} className="hover:underline hover:text-fuchsia-200">
                 <Link href={`/beer-recipes/${item.id}`} key={index}>
                 {item.name}
                 </Link>
             </li>
             )}
-        </ul>
+        </ol>
     </div>
 }
